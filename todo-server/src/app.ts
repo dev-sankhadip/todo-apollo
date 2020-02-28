43240b2f93ada3b3983910857394a4f7dbae87f1
sankhadip samanta <sankhsamanta.cse17@chitkarauniversity.edu.in>
2020-02-28 17:25:10 +0530

    server reorganised

--- todo-server/src/app.ts
+++ todo-server/src/app.ts
@@ -1,9 +1,30 @@
 import { ApolloServer,gql } from 'apollo-server';
 
 import { resolver as resolvers, schema as typeDefs } from './graphql/schema/index';
+import { getUser } from './security/auth'
 
-
-const server = new ApolloServer({ typeDefs, resolvers, cors:true });
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers, 
+  cors:true,
+  context:({ req })=>
+  {
+    const token=req.headers['authorization'];
+    if(!token)
+    {
+      return {
+        "user":null
+      }
+    }
+    else
+    {
+      let user=getUser(token);
+      return {
+        user
+      }
+    }
+  }
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
