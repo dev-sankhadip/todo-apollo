62e95cc2382cb8a8b47b6e4229bcaabf672b5539
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-04-05 17:18:43 +0530

    feat: code corrected

--- todo-client/src/components/auth/signup.js
+++ todo-client/src/components/auth/signup.js
@@ -1,56 +1,53 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import { gql } from 'apollo-boost';
 
 import { client } from '../../graphql/client';
 
 
-export const Signup=(props)=>
-{
-    const [ username, setUsername ]=useState('');
-    const [ email, setEmail ]=useState('');
-    const [password, setPassword]=useState('');
+export const Signup = (props) => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const setFormValue=(e, setter)=>
-    {
+    const setFormValue = (e, setter) => {
         setter(e.target.value);
     }
 
-    const submit=(e)=>
-    {
+    const submit = (e) => {
         e.preventDefault();
-        client.mutate({mutation:gql`
+        client.mutate({
+            mutation: gql`
             mutation{
                 createUser(UserInput:{username:"${username}",email:"${email}", password:"${password}"}){
                     code
                 }
             }
         `})
-        .then((res)=>
-        {
-            console.log(res);
-        })
-        .catch(err=>console.error(err))
+            .then((res) => {
+                console.log(res);
+            })
+            .catch(err => console.error(err))
     }
 
 
-    return(
+    return (
         <div className="container">
-            <form onSubmit={ submit }>
+            <form onSubmit={submit}>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
-                    <input type="email" className="form-control" id="username" onChange={(e)=>{ setFormValue(e, setUsername) }} />
+                    <input type="text" className="form-control" id="username" onChange={(e) => { setFormValue(e, setUsername) }} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="email">Email address</label>
-                    <input type="email" className="form-control" id="email" onChange={(e)=>{ setFormValue(e, setEmail) }} />
+                    <input type="email" className="form-control" id="email" onChange={(e) => { setFormValue(e, setEmail) }} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input type="password" className="form-control" id="password" onChange={(e)=>{ setFormValue(e, setPassword) }} />
+                    <input type="password" className="form-control" id="password" onChange={(e) => { setFormValue(e, setPassword) }} />
                 </div>
                 <button type="submit" className="btn btn-primary">Login</button>
             </form>
-            <p onClick={()=>{ props.history.push('/') }}>Login here</p>
+            <p onClick={() => { props.history.push('/') }}>Login here</p>
         </div>
     )
 }
--- todo-client/src/components/login.js
+++ todo-client/src/components/login.js
@@ -1,50 +1,54 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import { gql } from 'apollo-boost';
 
 import { client } from '../graphql/client'
 
 
-export const Login=(props)=>
-{
-    const [ email, setEmail ]=useState('');
-    const [ password, setPassword ]=useState('');
+export const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const submit=(e)=>
-    {
+    const submit = (e) => {
         e.preventDefault();
-        client.mutate({ mutation:gql`
+        client.mutate({
+            mutation: gql`
             mutation{
                 loginUser(email:"${email}", password:"${password}"){
                     code
+                    token
                 }
             }
         ` })
-        .then((res)=>
-        {
-            console.log(res)
-        })
-        .catch(err=>console.error(err))
+            .then((res) => {
+                console.log(res.data.loginUser.code)
+            })
+            .catch(err => console.error(err))
     }
 
-    const setFormValue=(e, setter)=>
-    {
+    const setFormValue = (e, setter) => {
         setter(e.target.value);
     }
 
-    return(
+    return (
         <div className="container">
-            <form onSubmit={ submit }>
-                <div className="form-group">
-                    <label htmlFor="email">Email address</label>
-                    <input type="email" className="form-control" id="email" onChange={(e)=>{ setFormValue(e, setEmail) }} />
+            <div className="row">
+                <div className="col-2"></div>
+                <div className="col-8">
+                    <form onSubmit={submit}>
+                        <div className="form-group">
+                            <label htmlFor="email">Email address</label>
+                            <input type="email" className="form-control" id="email" onChange={(e) => { setFormValue(e, setEmail) }} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" className="form-control" id="password" onChange={(e) => { setFormValue(e, setPassword) }} />
+                        </div>
+                        <button type="submit" className="btn btn-primary">Login</button>
+                    </form>
+                    <p onClick={() => { props.history.push('/signup') }} style={{ fontWeight:'bold' }}>Signup here</p>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" className="form-control" id="password" onChange={(e)=>{ setFormValue(e, setPassword) }} />
-                </div>
-                <button type="submit" className="btn btn-primary">Login</button>
-            </form>
-            <p onClick={()=>{ props.history.push('/signup') }}>Signup here</p>
+                <div className="col-2"></div>
+            </div>
         </div>
     )
 }
