a5e10eecad3797042535b6491d53d921ad9741e9
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-04-05 12:38:29 +0530

    feat: code updated

--- todo-server/src/graphql/schema/user/user.mutation.ts
+++ todo-server/src/graphql/schema/user/user.mutation.ts
@@ -1,33 +1,40 @@
-import jwt from 'jsonwebtoken';
-import shortid from 'shortid';
+import jwt from "jsonwebtoken";
+import shortid from "shortid";
+import { promisify } from "util";
 
-import { connection } from '../../../db/connection';
+import { connection, query } from "../../../db/connection";
 
-
-export const Mutation={
-    createUser: async(args, request)=>
-    {
-        console.log(args);
-        return{
-            code:200
-        }
-    },
-    loginUser: async (parent, args, context, info)=>
-    {
-        if(context.user!=null)
-        {
-            console.log(args);
-            const token = jwt.sign({ userId: '123', email: '123@gmail.com' }, 'todo', { expiresIn: '1h' });
-            console.log(token);
-            return{
-                code:200
-            }
-        }
-        else
-        {
-            return{
-                code:"401"
-            }
-        }
+export const Mutation = {
+  createUser: async (parent, args, context, info) => {
+    const uid = shortid.generate();
+    const { username, email, password } = args.UserInput;
+    query(`insert into users values('${uid}', '${username}', '${email}', '${password}')`)
+    .then(res=>{
+        console.log(res);
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+    return {
+      code: 200,
+    };
+  },
+  loginUser: async (parent, args, context, info) => {
+    if (context.user != null) {
+      console.log(args);
+      const token = jwt.sign(
+        { userId: "123", email: "123@gmail.com" },
+        "todo",
+        { expiresIn: "1h" }
+      );
+      console.log(token);
+      return {
+        code: 200,
+      };
+    } else {
+      return {
+        code: "401",
+      };
     }
-}
+  },
+};
