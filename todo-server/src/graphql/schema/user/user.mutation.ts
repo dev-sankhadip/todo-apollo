c05ff9dd5186813c13ee41e1714e82d8c33131d0
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-04-05 17:17:58 +0530

    feat: login code updated

--- todo-server/src/graphql/schema/user/user.mutation.ts
+++ todo-server/src/graphql/schema/user/user.mutation.ts
@@ -8,33 +8,57 @@ export const Mutation = {
   createUser: async (parent, args, context, info) => {
     const uid = shortid.generate();
     const { username, email, password } = args.UserInput;
-    query(`insert into users values('${uid}', '${username}', '${email}', '${password}')`)
-    .then(res=>{
+    return query(
+      `insert into users values('${uid}', '${username}', '${email}', '${password}')`
+    )
+      .then((res) => {
         console.log(res);
-    })
-    .catch(err=>{
-        console.log(err)
-    })
-    return {
-      code: 200,
-    };
+        return {
+          code: 200,
+        };
+      })
+      .catch((err) => {
+        console.log(err);
+        return {
+          code: 500,
+        };
+      });
   },
   loginUser: async (parent, args, context, info) => {
-    if (context.user != null) {
-      console.log(args);
-      const token = jwt.sign(
-        { userId: "123", email: "123@gmail.com" },
-        "todo",
-        { expiresIn: "1h" }
-      );
-      console.log(token);
-      return {
-        code: 200,
-      };
-    } else {
-      return {
-        code: "401",
-      };
-    }
+    // if (context.user != null) {
+    console.log(args);
+    const { email, password } = args;
+
+    return query(`select * from users where email = '${email}'`)
+      .then((res) => {
+        if (res[0].password === password) {
+          const token = jwt.sign(
+            { userId: res[0].id, email: res[0].email },
+            "todo",
+            {
+              expiresIn: "1h",
+            }
+          );
+          return {
+            code: 200,
+            token,
+          };
+        } else {
+          return {
+            code: 401,
+          };
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        return {
+          code: 500,
+        };
+      });
+    // } else {
+    //   return {
+    //     code: "401",
+    //   };
+    // }
   },
 };
